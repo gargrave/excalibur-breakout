eb113054317d3adbfd024ece84886c98d52f6b30
Gabe Hargrave <gkhargrave@gmail.com>
2019-06-09 13:24:14 -0700

    Move main game class into separate file

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,12 @@
-import * as ex from 'excalibur'
-
 import config from './config'
 
-import { Game, globals } from './core'
+import { globals } from './core'
 
+import Breakout from './game/Breakout'
 import GameScene from './game/scenes/GameScene'
 
-const game = new Game({
-  width: 800,
-  height: 600,
-})
+const game = new Breakout()
 globals.collGroups = config.collisionGroups
-game.backgroundColor = ex.Color.fromHex('#454545')
 
 game.add('gameScene', new GameScene(game))
 game.goToScene('gameScene')
